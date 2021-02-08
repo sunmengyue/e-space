17a7394d61af44ceec2cef82503f13740ccd111d
Simon <bardrabbit@gmail.com>
2021-02-08 11:27:23 -0500

    set up backend routes for products

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Espace</Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart mr-1'></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='./login'>
+                <Nav.Link>
+                  <i className='fas fa-user mr-1'></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
